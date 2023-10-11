3b3ae3e7273534645143d9849248d0201b82d208
Mekha1104 <gtrcooie@gmail.com>
2023-10-11 19:33:07 +0700

    .

--- Homework/HW12/hw12-3-structure-and-array.cpp
+++ Homework/HW12/hw12-3-structure-and-array.cpp
@@ -42,3 +42,54 @@
 */
 
 //TODO: #21 output money should in decimal format (eg. 120,200,340.42).
+#include <stdio.h>
+#include <string.h>
+
+struct Employee {
+    char Name[20];
+    float Salary;
+    int Duration;
+};
+
+int main() {
+    struct Employee employees[100];
+    int numEmployees = 0;
+    char choice;
+    float totalSalary = 0;
+    int maxDuration = 0;
+    int maxDurationIndex;
+
+    do {
+        printf("Do you want to Enter Employee Information? (y/n) : ");
+        scanf(" %c", &choice);
+        if (choice == 'y' || choice == 'Y') {
+            printf("----\n");
+            printf("Employee Name : ");
+            scanf("%s", employees[numEmployees].Name);
+            printf("Salary (Bath/Month) : ");
+            scanf("%f", &employees[numEmployees].Salary);
+            printf("Duration (Year) : ");
+            scanf("%d", &employees[numEmployees].Duration);
+            totalSalary += employees[numEmployees].Salary;
+            if (employees[numEmployees].Duration > maxDuration) {
+                maxDuration = employees[numEmployees].Duration;
+                maxDurationIndex = numEmployees;
+            }
+            numEmployees++;
+        }
+    } while (choice == 'y' || choice == 'Y');
+
+    // คำนวณเงินเดือนเฉลี่ยและการจ่ายเงินเดือนทั้งหมด
+    float averageSalary = totalSalary / numEmployees;
+    float totalPayment = totalSalary * numEmployees;
+
+    // แสดงผลลัพธ์
+    printf("Average of Salary : %.2f Bath\n", averageSalary);
+    printf("Payment of every month : %.2f Bath\n", totalPayment);
+    printf("----------------------------------------\n");
+    printf("** Most duration in this business **\n");
+    printf("Name : %s (%d Years)\n", employees[maxDurationIndex].Name, employees[maxDurationIndex].Duration);
+    printf("Salary : %.2f Bath\n", employees[maxDurationIndex].Salary);
+
+    return 0;
+}
